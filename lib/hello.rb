2a8f6f046e9839fc772135280bb11827417de2bc
Derek Barncard <derek@barncard.com>
2014-09-07 13:03:45 -0700

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
 # Default is "World"
 # Author: Derek Barncard (derek@barncard.com)
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
